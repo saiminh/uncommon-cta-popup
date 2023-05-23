409b94e5d373461093dd145a31e6cc352c369bca
saiminh <saiminh@gmail.com>
2023-05-23 17:35:05 +0200

    editor styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '226d38819955fdb8b377');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c735b2ef176f0823a3ca');
